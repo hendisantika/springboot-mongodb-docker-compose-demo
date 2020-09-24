2f332f3559b382ba8be7257f9994ad20505c8141
Hendi Santika <hendisantika@yahoo.co.id>
2020-09-24 20:11:09 +0700

    #10 - Add swaggerPersonApi10 bean in SwaggerConfig

--- src/main/java/com/hendisantika/studentservice/config/SwaggerConfig.java
+++ src/main/java/com/hendisantika/studentservice/config/SwaggerConfig.java
@@ -1,9 +1,17 @@
 package com.hendisantika.studentservice.config;
 
+import com.google.common.collect.Sets;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 /**
  * Created by IntelliJ IDEA.
@@ -30,4 +38,19 @@ public class SwaggerConfig {
             "DemoRest 2.0", "Demo Rest Api", "2.0",
             "urn:tos", DEFAULT_CONTACT,
             "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0");
+
+
+    @Bean
+    public Docket swaggerPersonApi10() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("demorestapi-1.0")
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.hendisantika.studentservice.controller"))
+                .paths(PathSelectors.ant("/api/v1/**"))
+                .build()
+                //.pathMapping("/api/v1")
+                .produces(Sets.newHashSet(APPLICATION_JSON_VALUE))
+                .consumes(Sets.newHashSet(APPLICATION_JSON_VALUE))
+                .apiInfo(V1_API_INFO);
+    }
 }
