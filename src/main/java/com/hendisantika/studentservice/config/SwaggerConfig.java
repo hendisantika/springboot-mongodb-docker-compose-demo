fb45a35ed39369f644e0ab8c41f742020a0f3fd2
hendisantika <hendisantika@yahoo.co.id>
2024-03-31 12:43:39 +0700

    #109 - Modify Spring Doc ✏️📗 :octocat: 🐳⬆

--- src/main/java/com/hendisantika/studentservice/config/SwaggerConfig.java
+++ src/main/java/com/hendisantika/studentservice/config/SwaggerConfig.java
@@ -1,17 +1,13 @@
 package com.hendisantika.studentservice.config;
 
-import com.google.common.collect.Sets;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,48 +19,33 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
  * Time: 20.09
  */
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
 
-    public static final Contact DEFAULT_CONTACT = new Contact(
-            "Hendi Santika", "https://github.com/hendisantika", "hendisantika@yahoo.co.id");
-
-    public static final ApiInfo V1_API_INFO = new ApiInfo(
-            "DemoRest 1.0", "Demo Rest Api", "1.0",
-            "urn:tos", DEFAULT_CONTACT,
-            "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0");
-
-    public static final ApiInfo V2_API_INFO = new ApiInfo(
-            "DemoRest 2.0", "Demo Rest Api", "2.0",
-            "urn:tos", DEFAULT_CONTACT,
-            "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0");
-
-
     @Bean
-    public Docket swaggerPersonApi10() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .groupName("demorestapi-1.0")
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.hendisantika.studentservice.controller"))
-                .paths(PathSelectors.ant("/api/v1/**"))
-                .build()
-                //.pathMapping("/api/v1")
-                .produces(Sets.newHashSet(APPLICATION_JSON_VALUE))
-                .consumes(Sets.newHashSet(APPLICATION_JSON_VALUE))
-                .apiInfo(V1_API_INFO);
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("student-service")
+                .pathsToMatch("/**")
+                .build();
     }
 
     @Bean
-    public Docket swaggerPersonApi20() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .groupName("demorestapi-2.0")
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.hendisantika.studentservice.controller"))
-                .paths(PathSelectors.ant("/api/v2/**"))
-                .build()
-                //.pathMapping("/api/v2")
-                .produces(Sets.newHashSet(APPLICATION_JSON_VALUE))
-                .consumes(Sets.newHashSet(APPLICATION_JSON_VALUE))
-                .apiInfo(V2_API_INFO);
+    public OpenAPI customOpenAPI(@Value("${application-description}") String appDescription, @Value("${application" +
+            "-version}") String appVersion) {
+        Contact contact = new Contact();
+        contact.setEmail("hendisantika@yahoo.co.id");
+        contact.setName("Hendi Santika");
+        contact.setUrl("https://www.s.id/hendisantika");
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Student Service Application API")
+                        .version(appVersion)
+                        .description(appDescription)
+                        .termsOfService("http://swagger.io/terms/")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org"))
+                        .contact(contact)
+                );
+
     }
+
 }
